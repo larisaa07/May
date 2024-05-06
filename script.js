7a143c326d57f9e56d62e3e8b107c64cbf6de9bd
larisaa07 <larisa_aveti@yahoo.com>
2024-05-06 13:41:57 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+//team member data
+const teamMembers = [
+    {
+        name: 'Lionel Messi',
+        age: 36,
+        activePlayer: true,
+        position: 'forward',
+        strengths: 'ball control',
+        weaknesses: 'heading ability',
+        skills: ['dribbling', 'shooting', 'passing'],
+        biography: 'One of the greatest players of all time'
+    }
+]
+
+function generateTeamCards(){
+    const teamCardsContainer = document.getElementById('teamCards');
+
+    teamMembers.forEach(member => {
+        const card = document.createElement('div')
+        card.classList.add('col-md-4')
+
+        card.innerHTML=`
+            <div class="card">
+                <div class = "card-header">
+                    ${member.name}
+                </div>
+                <div class="card-body">
+                    <p><strong>Position:</strong> ${member.position}</p>
+                </div>
+            </div>
+
+        `
+    teamCardsContainer.appendChild(card)
+    });
+}
+
+window.onload = generateTeamCards()
