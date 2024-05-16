ea02e1ce8be3a1ffc88e43fd705043f4f977d41c
larisaa07 <larisa_aveti@yahoo.com>
2024-05-16 14:39:55 -0400

    Added all olympians data + tried to make bg color change per name (fix)

--- script.js
+++ script.js
@@ -1,14 +1,124 @@
 //team member data
 const teamMembers = [
     {
-        name: 'Lionel Messi',
-        age: 36,
-        activePlayer: true,
-        position: 'forward',
-        strengths: 'ball control',
-        weaknesses: 'heading ability',
-        skills: ['dribbling', 'shooting', 'passing'],
-        biography: 'One of the greatest players of all time'
+        name: 'Zeus',
+        romanName: 'Jupiter',
+        titleDomain: ['King of the gods', 'Ruler of Olympus', 'god of the sky, lightning, thunder, law, order, and justice'],
+        father: 'Cronus',
+        mother: 'Rhea',
+        symbols: ['thunderbolt', 'eagle', 'oak tree', 'bull', 'scepter', 'scales'],
+        spouce: true,
+        spouceName: 'Hera',
+    }
+    {
+        name: 'Hera',
+        romanName: 'Juno',
+        titleDomain: ['Queen of the gods', 'goddess of marriage, women, childbirth, and family'],
+        father: 'Cronus',
+        mother: 'Rhea',
+        symbols: ['peacock', 'cuckoo', 'cow'],
+        spouce: true,
+        spouceName: 'Zeus',
+    }
+    {
+        name: 'Poseidon',
+        romanName: 'Neptune',
+        titleDomain: ['Earthshaker', 'god of the seas, water, storms, hurricanes, earthquakes, and horses'],
+        father: 'Cronus',
+        mother: 'Rhea',
+        symbols: ['trident', 'horse', 'bull', 'dolphin'],
+        spouce: true,
+        spouceName: 'Amphitrite (Nereid)',
+    }
+    {
+        name: 'Demeter',
+        romanName: 'Ceres',
+        titleDomain: ['goddess of harest, fertility, agriculture, nature, and the seasons'],
+        father: 'Cronus',
+        mother: 'Rhea',
+        symbols: ['poppy', 'wheat', 'torch', 'cornucopia', 'pig'],
+        spouce: false,
+        spouceName: 'N/A',
+    }
+    {
+        name: 'Athena',
+        romanName: 'Minerva',
+        titleDomain: ['goddess of wisdom, handicraft, and warfare'],
+        father: 'Zeus',
+        mother: 'Metis (Oceanid)',
+        symbols: ['owl', 'olive tree'],
+        spouce: false,
+        spouceName: 'N/A',
+    }
+    {
+        name: 'Hephaestus',
+        romanName: 'Vulcan',
+        titleDomain: ['Master blacksmith and craftsman of the gods', 'god of the forge, craftsmanship, invention, fire, and volcanoes'],
+        father: 'Zeus (or N/A - born of asexual reproduction)',
+        mother: 'Hera',
+        symbols: ['fire', 'anvil', 'axe', 'donkey', 'hammer', 'tongs', 'quail'],
+        spouce: true,
+        spouceName: 'Aphrodite',
+    }
+    {
+        name: 'Ares',
+        romanName: 'Mars',
+        titleDomain: ['god of war, violence, bloodshed, and manly virtues'],
+        father: 'Zeus',
+        mother: 'Hera',
+        symbols: ['boar', 'serpent', 'dog', 'vulture', 'spear', 'sheild'],
+        spouce: false,
+        spouceName: 'N/A (in a relationship w/ Aphrodite)',
+    }
+    {
+        name: 'Aphrodite',
+        romanName: 'Venus',
+        titleDomain: ['goddess of love, pleasure, passion, procreation, fertility, beauty, and desire'],
+        father: 'Zeus',
+        mother: 'Dione (Oceanid or Titaness)',
+        symbols: ['dove', 'bird', 'apple', 'bee', 'swan', 'myrtle', 'rose'],
+        spouce: true,
+        spouceName: 'Hephaestus',
+    }
+    {
+        name: 'Artemis',
+        romanName: 'Diana',
+        titleDomain: ['goddess of the hunt, the wilderness, virginity, the moon, archery, childbirth, protection, and plague'],
+        father: 'Zeus',
+        mother: 'Leto',
+        symbols: ['the moon', 'horse', 'deer', 'hound', 'she-bear', 'cypress tree', 'bow and arrow'],
+        spouce: false,
+        spouceName: 'N/A',
+    }
+    {
+        name: 'Apollo',
+        romanName: 'Apollo',
+        titleDomain: ['god of the sun, light, prophecy, philosophy, archery, truth, inspiration, poetry, music, arts, manly beauty, medicine, healing, and plague'],
+        father: 'Zeus',
+        mother: 'Leto',
+        symbols: ['lyre', 'raven', 'swan', 'wolf', 'bow and arrow'],
+        spouce: false,
+        spouceName: 'N/A',
+    }
+    {
+        name: 'Hermes',
+        romanName: 'Mercury',
+        titleDomain: ['Messenger of the gods', 'Guide of dead souls', 'god of travel, commerce, communication, borders, eloquence, diplomacy, thieves, and games'],
+        father: 'Zeus',
+        mother: 'Maia (Nymph)',
+        symbols: ['the caduceus (his staff with 2 snakes on it)', 'winged sandals', 'stork', 'tortoise'],
+        spouce: false,
+        spouceName: 'N/A',
+    }
+    {
+        name: 'Dionysus',
+        romanName: 'Bacchus',
+        titleDomain: ['god of wine, the grapevine, fertility, festivity, ecstasy, madness, and resurrection', 'patron god of arts and theater'],
+        father: 'Zeus',
+        mother: 'Semele (mortal Theban princess)',
+        symbols: ['grapevine', 'ivy', 'cup', 'tiger', 'leopard', 'dolphin', 'goat', 'pinecone'],
+        spouce: true,
+        spouceName: 'Aridne (Cretan princess)',
     }
 ]
 
@@ -21,15 +131,42 @@ function generateTeamCards(){
 
         //style the GB color of the cards based on person
         let backgroundColor
-        switch(member.position.toLowerCase()) {
-            case 'forward':
+        switch(member.name.toLowerCase()) {
+            case 'zeus':
+                backgroundColor = 'yellow'
+                break
+            case 'hera':
+                backgroundColor = 'gray'
+                break
+            case 'poseidon':
+                backgroundColor = 'blue'
+                break
+            case 'demeter':
                 backgroundColor = 'red'
                 break
-            case 'mid':
-                backgroundColor = 'green'
+            case 'athena':
+                backgroundColor = 'gray'
                 break
-            case 'defender':
-                backgroundColor = 'blue'
+            case 'hephaestus':
+                backgroundColor = 'orange'
+                break
+            case 'ares':
+                backgroundColor = 'red'
+                break
+            case 'aphrodite':
+                backgroundColor = 'pink'
+                break
+            case 'artemis':
+                backgroundColor = 'gray'
+                break
+            case 'apollo':
+                backgroundColor = 'yellow'
+                break
+            case 'hermes':
+                backgroundColor = 'orange'
+                break
+            case 'dionysus':
+                backgroundColor = 'purple'
                 break
             default:
                 backgroundColor = 'gray'
@@ -38,7 +175,7 @@ function generateTeamCards(){
 
         card.innerHTML=`
             <div class="card">
-                <div class = "card-header">
+                <div   class = "card-header">
                     ${member.name}
                 </div>
                 <div class="card-body" style = "background-color:${backgroundColor};">
